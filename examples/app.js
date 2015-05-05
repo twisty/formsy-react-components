cd1439ef5b440ee9987a780e691eaedf5a7bc8a5
Tim Brayshaw <tim@brayshaw.com>
2015-05-05 21:46:46 +0100

    Add examples of date and password types. Add fieldsets to impose a bit of order.

--- examples/app.js
+++ examples/app.js
@@ -109,82 +109,131 @@ var Examples = React.createClass({
                     <h2>Layout: <code>{this.state.layout}</code></h2>
                 </div>
                 <Formsy.Form className={formClassName} ref="form">
-                    <Textarea
-                        name="txtArea1"
-                        label="Textarea"
-                        layout={this.state.layout}
-                        validatePristine={this.state.validatePristine}
-                        disabled={this.state.disabled}
-                        placeholder="This field requires 10 characters."
-                        help="This is some help text for the textarea."
-                        validations="minLength:10"
-                        validationErrors={{
-                            minLength: 'Please provide at least 10 characters.'
-                        }}
-                    />
-                    <Input
-                        name="textInput1"
-                        value=""
-                        label="Text"
-                        type="text"
-                        layout={this.state.layout}
-                        validatePristine={this.state.validatePristine}
-                        disabled={this.state.disabled}
-                        placeholder="Here is a text input."
-                        help="This is a required text input."
-                        required
-                    />
-                    <Input
-                        name="textInput2"
-                        value=""
-                        label="Email"
-                        type="email"
-                        layout={this.state.layout}
-                        validatePristine={this.state.validatePristine}
-                        disabled={this.state.disabled}
-                        placeholder="This is an email input."
-                    />
-                    <Select
-                        name="select1"
-                        label="Select"
-                        layout={this.state.layout}
-                        validatePristine={this.state.validatePristine}
-                        disabled={this.state.disabled}
-                        help="This is a required select element."
-                        options={selectOptions}
-                        required
-                    />
-                    <RadioGroup
-                        name="radioGrp1"
-                        value="b"
-                        label="Radio group (stacked)"
-                        layout={this.state.layout}
-                        validatePristine={this.state.validatePristine}
-                        disabled={this.state.disabled}
-                        help="Here, “Option B” is initially selected."
-                        options={radioOptions}
-                    />
-                    <RadioGroup
-                        name="radioGrp2"
-                        type="inline"
-                        label="Radio group (inline)"
-                        layout={this.state.layout}
-                        validatePristine={this.state.validatePristine}
-                        disabled={this.state.disabled}
-                        help="This is a required radio group."
-                        options={radioOptions}
-                        required
-                    />
-                    <RadioGroup
-                        name="radioGrp3"
-                        type="inline"
-                        label="Radio group (disabled)"
-                        layout={this.state.layout}
-                        validatePristine={this.state.validatePristine}
-                        disabled={this.state.disabled}
-                        help="Here, “Option B” is disabled."
-                        options={radioOptionsDisabled}
-                    />
+                    <fieldset>
+                        <legend>Input types</legend>
+                        <Input
+                            name="text1"
+                            value=""
+                            label="Text"
+                            type="text"
+                            layout={this.state.layout}
+                            validatePristine={this.state.validatePristine}
+                            disabled={this.state.disabled}
+                            placeholder="Here is a text input."
+                            help="This is a required text input."
+                            required
+                        />
+                        <Input
+                            name="date1"
+                            value=""
+                            label="Date"
+                            type="date"
+                            layout={this.state.layout}
+                            validatePristine={this.state.validatePristine}
+                            disabled={this.state.disabled}
+                            placeholder="This is a date input."
+                            required
+                        />
+                        <Input
+                            name="email1"
+                            value=""
+                            label="Email"
+                            type="email"
+                            layout={this.state.layout}
+                            validatePristine={this.state.validatePristine}
+                            disabled={this.state.disabled}
+                            placeholder="This is an email input."
+                        />
+                        <Input
+                            name="password1"
+                            value=""
+                            label="Password"
+                            type="password"
+                            layout={this.state.layout}
+                            validatePristine={this.state.validatePristine}
+                            validations="minLength:8"
+                            validationError="Your password must be at least 8 characters long."
+                            disabled={this.state.disabled}
+                            placeholder="Choose a password"
+                        />
+                        <Input
+                            name="password2"
+                            value=""
+                            label="Confirm password"
+                            type="password"
+                            layout={this.state.layout}
+                            validatePristine={this.state.validatePristine}
+                            disabled={this.state.disabled}
+                            validations="equalsField:password1"
+                            validationErrors={{
+                                equalsField: 'Passwords must match.'
+                            }}
+                            placeholder="Retype password"
+                        />
+                    </fieldset>
+                    <fieldset>
+                        <legend>Textarea</legend>
+                        <Textarea
+                            name="txtArea1"
+                            label="Textarea"
+                            layout={this.state.layout}
+                            validatePristine={this.state.validatePristine}
+                            disabled={this.state.disabled}
+                            placeholder="This field requires 10 characters."
+                            help="This is some help text for the textarea."
+                            validations="minLength:10"
+                            validationErrors={{
+                                minLength: 'Please provide at least 10 characters.'
+                            }}
+                        />
+                    </fieldset>
+                    <fieldset>
+                        <legend>Select</legend>
+                        <Select
+                            name="select1"
+                            label="Select"
+                            layout={this.state.layout}
+                            validatePristine={this.state.validatePristine}
+                            disabled={this.state.disabled}
+                            help="This is a required select element."
+                            options={selectOptions}
+                            required
+                        />
+                    </fieldset>
+                    <fieldset>
+                        <legend>Radio group</legend>
+                        <RadioGroup
+                            name="radioGrp1"
+                            value="b"
+                            label="Radio group (stacked)"
+                            layout={this.state.layout}
+                            validatePristine={this.state.validatePristine}
+                            disabled={this.state.disabled}
+                            help="Here, “Option B” is initially selected."
+                            options={radioOptions}
+                        />
+                        <RadioGroup
+                            name="radioGrp2"
+                            type="inline"
+                            label="Radio group (inline)"
+                            layout={this.state.layout}
+                            validatePristine={this.state.validatePristine}
+                            disabled={this.state.disabled}
+                            help="This is a required radio group."
+                            options={radioOptions}
+                            required
+                        />
+                        <RadioGroup
+                            name="radioGrp3"
+                            type="inline"
+                            label="Radio group (disabled)"
+                            layout={this.state.layout}
+                            validatePristine={this.state.validatePristine}
+                            disabled={this.state.disabled}
+                            help="Here, “Option B” is disabled."
+                            options={radioOptionsDisabled}
+                        />
+                    </fieldset>
                     <Row layout={this.state.layout}>
                         <input className="btn btn-default" onClick={this.resetForm} type="reset" defaultValue="Reset" />
                         {' '}
