a8052b9b4daff22475c2f98ee0699bf208dc9452
Tim Brayshaw <tim@brayshaw.com>
2015-05-11 13:18:45 +0100

    Add package.json, and main.js.

---
+++ src/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+    Input: require('./input'),
+    Textarea: require('./textarea'),
+    Select: require('./select'),
+    RadioGroup: require('./radio-group')
+};
