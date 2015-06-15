c6ecbacbf935e8edb1e71abca1f8183f9d9185a2
Tim Brayshaw <tim@brayshaw.com>
2015-06-15 11:32:55 +0100

    Split file input into own element, and return a filelist instead of the element's value. Closes #10.

---
+++ src/input-file.js
@@ -0,0 +1,72 @@
+/*jshint node:true */
+
+'use strict';
+
+var React = require('react');
+var Formsy = require('formsy-react');
+var ComponentMixin = require('./mixins/component');
+var Row = require('./row');
+var Icon = require('./icon');
+
+var File = React.createClass({
+
+    mixins: [Formsy.Mixin, ComponentMixin],
+
+    getInitialState: function() {
+        return {
+            fileList: []
+        };
+    },
+
+    changeValue: function(event) {
+        var target = event.currentTarget;
+        var value = target.value;
+        this.setState({fileList: target.files});
+        this.setValue(target.files);
+        this.props.onChange(this.props.name, target.files, value);
+    },
+
+    render: function() {
+        var element = this.renderElement();
+
+        if (this.getLayout() === 'elementOnly' || this.props.type === 'hidden') {
+            return element;
+        }
+
+        var warningIcon = '';
+        if (this.showErrors()) {
+            warningIcon = (
+                <Icon symbol="remove" className="form-control-feedback" />
+            );
+        }
+
+        return (
+            <Row
+                label={this.props.label}
+                required={this.isRequired()}
+                hasErrors={this.showErrors()}
+                layout={this.getLayout()}
+            >
+                {element}
+                {warningIcon}
+                {this.renderHelp()}
+                {this.renderErrorMessage()}
+            </Row>
+        );
+    },
+
+    renderElement: function() {
+        return (
+            <input
+                {...this.props}
+                type="file"
+                label={null}
+                onChange={this.changeValue}
+                disabled={this.isFormDisabled() || this.props.disabled}
+            />
+        );
+    }
+
+});
+
+module.exports = File;
