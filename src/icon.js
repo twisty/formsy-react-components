eda8a31e7abfaf14c54161c65b1822125b33fc5e
Tim Brayshaw <tim@brayshaw.com>
2016-03-31 21:36:20 +0100

    Convert to stateless function.

--- src/icon.js
+++ src/icon.js
@@ -1,25 +1,21 @@
-'use strict';
+import React, { PropTypes } from 'react';
 
-var React = require('react');
+const Icon = (props) => {
+    let classNames = [
+        'glyphicon',
+        'glyphicon-' + props.symbol
+    ];
+    if (props.className) {
+        classNames.push(props.className);
+    };
+    return (
+        <span className={classNames.join(' ')} aria-hidden="true"></span>
+    );
+}
 
-var Icon = React.createClass({
-
-    requiredProps: {
-        symbol: React.PropTypes.string.isRequired,
-        className: React.PropTypes.string
-    },
-
-    defaultProps: {
-        className: ''
-    },
-
-    render: function() {
-        var className = 'glyphicon glyphicon-' + this.props.symbol + ' ' + this.props.className;
-        return (
-            <span className={className} aria-hidden="true"></span>
-        );
-    }
-
-});
+Icon.propTypes = {
+    symbol: PropTypes.string.isRequired,
+    className: PropTypes.string
+};
 
 module.exports = Icon;
