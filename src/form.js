8342aa418b9eea790a541324fc8c2f5bfde47c92
Tim Brayshaw <tim@brayshaw.com>
2017-01-19 10:29:08 +0000

    Don't pass consumed props on to formsy.

--- src/form.js
+++ src/form.js
@@ -11,10 +11,17 @@ var Form = React.createClass({
     },
 
     render() {
+        let formsyProps = Object.assign({}, this.props);
+        delete formsyProps.layout;
+        delete formsyProps.validateOnSubmit;
+        delete formsyProps.validatePristine;
+        delete formsyProps.rowClassName;
+        delete formsyProps.labelClassName;
+        delete formsyProps.elementWrapperClassName;
         return (
             <Formsy.Form
                 className={this.getLayoutClassName()}
-                {...this.props}
+                {...formsyProps}
                 ref="formsy"
             >
                 {this.props.children}
