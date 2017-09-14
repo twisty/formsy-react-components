0a53d95178787b76506108a55284af089f810015
Tim Brayshaw <tim@brayshaw.com>
2017-09-14 15:30:53 +0100

    Use prettier + airbnb lint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,18 @@
-{
-  "parser": "babel-eslint",
-  "env": {
-    "es6": true
-  },
-  "plugins": [
-    "react"
-  ],
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  "rules": {
-    "react/sort-prop-types": [2, {
-      "callbacksLast": true,
-      "requiredFirst": true
-    }],
-    "react/prefer-stateless-function": "warn"
+module.exports = {
+  parser: "babel-eslint",
+  extends: ["airbnb", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": ["error", "fb"],
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "jsx-a11y/label-has-for": [
+      2,
+      {
+        required: {
+          some: ["nesting", "id"]
+        },
+        allowChildren: false
+      }
+    ]
   }
-}
+};
