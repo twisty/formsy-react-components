f62cdee79f795a0776518515a47129e046708ba9
Tim Brayshaw <tim@brayshaw.com>
2017-09-15 11:09:13 +0100

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,28 @@
-/*eslint-env node */
+/* eslint-env node */
 
-var path = require('path');
+const path = require('path');
 
 module.exports = {
+  devtool: 'source-map',
 
-    devtool: 'source-map',
+  entry: {
+    playground: path.resolve(__dirname, './playground/src/index.js'),
+    refs: path.resolve(__dirname, './refs/src/index.js'),
+  },
 
-    entry: {
-        playground: path.resolve(__dirname, './playground/src/index.js'),
-        refs: path.resolve(__dirname, './refs/src/index.js')
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/build',
+  },
+
+  module: {
+    loaders: [{test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}],
+  },
+
+  resolve: {
+    alias: {
+      'formsy-react-components': path.resolve(__dirname, '../src/main'),
     },
-
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/build'
-    },
-
-    module: {
-        loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
-        ]
-    },
-
-    resolve: {
-        alias: {
-            'formsy-react-components': path.resolve(__dirname, '../src/main')
-        }
-    }
-
+  },
 };
