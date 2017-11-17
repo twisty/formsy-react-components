da5a6aedd444f05975f6ed51bb56f9d5d62fd4df
Tim Brayshaw <tim@brayshaw.com>
2017-11-17 22:06:45 +0000

    feat: Use React 16.

--- src/components/__tests__/test-helper.js
+++ src/components/__tests__/test-helper.js
@@ -1,4 +1,5 @@
+import 'raf/polyfill';
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({adapter: new Adapter()});
