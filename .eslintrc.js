043b749614637a8a30acc48ac6d820699fd81f45
Tim Brayshaw <tim@brayshaw.com>
2018-10-25 11:26:57 +0100

    Upgrade eslint-plugin-prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,13 @@ module.exports = {
   extends: ["airbnb", "prettier", "prettier/react"],
   plugins: ["prettier"],
   rules: {
-    "prettier/prettier": ["error", "fb"],
+    "prettier/prettier": ["error", {
+      singleQuote: true,
+      trailingComma: 'all',
+      bracketSpacing: false,
+      jsxBracketSameLine: true,
+      parser: 'flow'
+    }],
     "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
     "jsx-a11y/label-has-for": "off",
     'jsx-a11y/label-has-associated-control': ['error', {
