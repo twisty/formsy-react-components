53815e3803e2b2f72d2faa9836e44f57253dfb9c
Tim Brayshaw <tim@twisty.com>
2019-03-03 00:19:03 +0000

    Fix timer recursion with lodash.debounce in jest (#130)
    * Switch our debounce function to a simpler variant.
    * Improve readability of input test.

--- src/components/input.js
+++ src/components/input.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import debounce from 'lodash.debounce';
+import debounce from '../debounce';
 import {componentPropTypes, componentDefaultProps} from './component-common';
 import ErrorMessages from './error-messages';
 import Help from './help';
