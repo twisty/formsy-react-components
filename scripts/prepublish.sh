9e78738c0667ea199d6b6055c9742227ef208981
Tim Brayshaw <tim@brayshaw.com>
2019-03-18 09:50:47 +0000

    Update prepublish safeguard script.

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ $(git rev-parse --abbrev-ref HEAD) != "master" ]]; then
-  echo "Switch to master branch before publishing.";
+if [[ $(git rev-parse --abbrev-ref HEAD) != "release-1.x" ]]; then
+  echo "Switch to release-1.x branch before publishing.";
   exit 1;
 fi
 
