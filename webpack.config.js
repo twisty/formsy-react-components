ce5dfa4b924c82c6c944bbec8e08d243bad11906
Tim Brayshaw <tim@brayshaw.com>
2020-03-11 13:49:03 +0000

    Build in umd format.

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+/* eslint-env node */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const {TsconfigPathsPlugin} = require('tsconfig-paths-webpack-plugin');
+
+const config = {
+  devtool: 'source-map',
+
+  entry: {
+    main: './src',
+  },
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist-umd'),
+    library: 'formsy-react-components',
+    libraryTarget: 'umd',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+
+  externals: {
+    'formsy-react': {
+      root: 'formsy-react',
+      commonjs2: 'formsy-react',
+      commonjs: 'formsy-react',
+      amd: 'formsy-react',
+    },
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    },
+  },
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'production') {
+    config.output.filename = '[name].min.js';
+  }
+  return config;
+};
